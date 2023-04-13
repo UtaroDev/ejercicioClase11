bdadc14c3ece3d0dc8a1cd3a91592bef31417bc3
lautaro ruciani <lautaroruciani@gmail.com>
2023-04-13 16:50:07 -0300

    dos archivos sin terminar

---
+++ ejempmethod.js
@@ -0,0 +1,14 @@
+const array=[1,2,3,4,5];
+
+const Callback1 = (num) => { 
+    if (num>2) {
+        return ("este numero es mayor a 2", $num);
+    }
+ };
+
+console.log (Callback1(array));
+
+
+
+
+//const encontrarNum = array.find(Callback1);
---
+++ ejer2.js
@@ -0,0 +1,6 @@
+//¡Atención, equipo de inteligencia! Hemos recibido un reporte de un importante hackeo en nuestra red de computadoras. Necesitamos su ayuda para encontrar al responsable del ataque y detenerlo antes de que cause más 
+//daño. Se sabe que el hacker es parte de una lista de los 10 delincuentes más buscados. Además sabemos que tiene fanatismo por dejar “su marca” en los registros, creemos que se llama Juan o Pedro, y que sus números 
+//favoritos son el 3 y el 7. Encuentren al hacker lo más rápido que puedan, el destino de todos está en sus manos. 
+
+const wantedList=['Juan', 'Santiago', 'Luis', 'Carlos', 'Miguel', 'Jorge', 'Fernando', 'Pedro', 'Andrés', 'Daniel'];
+
