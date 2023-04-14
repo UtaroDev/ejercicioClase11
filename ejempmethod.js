be59327e1d04a9bd59f124f4f2b96748011a1e10
lautaro ruciani <lautaroruciani@gmail.com>
2023-04-14 13:09:29 -0300

    Ejercicio 2 terminado, ignorar el archivo ejempmethod.js

--- ejempmethod.js
+++ ejempmethod.js
@@ -1,14 +1,11 @@
-const array=[1,2,3,4,5];
+const array=[1,2,3,4,10,25,12];
 
-const Callback1 = (num) => { 
-    if (num>2) {
-        return ("este numero es mayor a 2", $num);
-    }
- };
+ /*
+const Callback1 = (num,index) => { 
+        console.log (index)
+        return num>4&& index>3
+ }; 
 
-console.log (Callback1(array));
-
-
-
-
-//const encontrarNum = array.find(Callback1);
+  */
+ const encontrarNum = array.find((num, index)=> num>4 && index>5);
+ console.log ("El numero es", encontrarNum);
